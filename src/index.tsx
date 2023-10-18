21368bfc2465102bb4c373e2691d988a2e4b7558
Milton Dias <milton.soares@cbm.df.gov.br>
2023-10-18 14:35:58 -0300

    Configuração do STYLE na LISTA, BOTÃO e FORMULÁRIO

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './pages/App'
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+   document.getElementById('root') as HTMLElement
+)
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+)
 
 /* 
 FORMA ANTIGA ATÉ REACT 17
